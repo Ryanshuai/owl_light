ac2eb263f1d34b958f5f8b04f2431086fb945558
Ryan <1018718155@qq.com>
2021-08-20 13:05:18 +0800

    add modes

--- code.py
+++ code.py
@@ -14,10 +14,11 @@ from rainbow import angle_to_rgb
 # Update this to match the number of NeoPixel LEDs connected to your board.
 num_pixels = 8
 pixels_0 = neopixel.NeoPixel(board.GP0, num_pixels, auto_write=False)
-pixels_0.brightness = 0.5
 pixels_1 = neopixel.NeoPixel(board.GP1, num_pixels, auto_write=False)
-pixels_1.brightness = 0.5
 pixels_2 = neopixel.NeoPixel(board.GP2, num_pixels, auto_write=False)
+
+pixels_0.brightness = 0.5
+pixels_1.brightness = 0.5
 pixels_2.brightness = 0.5
 
 button1 = digitalio.DigitalInOut(board.GP6)
@@ -26,52 +27,51 @@ button1.switch_to_input(pull=digitalio.Pull.UP)
 button2 = digitalio.DigitalInOut(board.GP7)
 button2.switch_to_input(pull=digitalio.Pull.UP)
 
-# pixels_0.fill((255, 0, 0))
-# while True:
-#     for i in range(-255, 255):
-#         pixels_0.brightness = abs(i) / 255
-#         pixels_0.show()
-#         time.sleep(0.01)
+state = 7
 
 
-state = 0
+def button_func(button1_value, button2_value, frozen=False):
+    global state
+    if button1_value == 0 and button2_value == 0:
+        pass
+        return True
+    if button1_value == 0 or button2_value == 0:
+        if not frozen:
+            state += button1.value - button2.value
+        return True
+    return False
+
+
 while True:
-    state %= 4
+    state = min(max(state, -1), 8)
+    print("state:", state)
+
     if state == 0:
+        pixels_0.fill((255, 100, 5))
+        pixels_1.fill((255, 100, 5))
+        pixels_2.fill((255, 100, 5))
+        for i in range(-255, 255):
+            pixels_0.brightness = abs(i) / 255 / 2
+            pixels_1.brightness = abs(i) / 255 / 2
+            pixels_2.brightness = abs(i) / 255 / 2
+            pixels_0.show()
+            pixels_1.show()
+            pixels_2.show()
+            time.sleep(0.01)
+
+            if button_func(button1.value, button2.value):
+                break
+
+    elif state == 1:
         for i in range(360):
             pixels_0.fill(angle_to_rgb(i / 360))
+            pixels_1.fill(angle_to_rgb(i / 360))
+            pixels_2.fill(angle_to_rgb(i / 360))
             pixels_0.show()
-            pixels_1.fill(angle_to_rgb((i) / 360))
             pixels_1.show()
-            pixels_2.fill(angle_to_rgb((i) / 360))
             pixels_2.show()
             time.sleep(0.0001)
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
-                break
-
-
-    if state == 1:
-        pixels_0.fill((255, 0, 0))
-        pixels_1.fill((255, 0, 0))
-        pixels_2.fill((255, 0, 0))
-        for i in range(-255, 255):
-            pixels_0.brightness = abs(i) / 255
-            pixels_0.show()
-            pixels_1.brightness = abs(i) / 255
-            pixels_1.show()
-            pixels_2.brightness = abs(i) / 255
-            pixels_2.show()
-            time.sleep(0.1)
-
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
+            if button_func(button1.value, button2.value):
                 break
 
     elif state == 2:
@@ -81,30 +81,104 @@ while True:
 
         for i in range(-255, 255):
             pixels_0.brightness = abs(i) / 255
-            pixels_0.show()
             pixels_1.brightness = abs(i) / 255
-            pixels_1.show()
             pixels_2.brightness = abs(i) / 255
+            pixels_0.show()
+            pixels_1.show()
+            pixels_2.show()
+            time.sleep(0.001)
+
+            if button_func(button1.value, button2.value):
+                break
+
+    elif state == 3:
+        for i in range(360):
+            pixels_0.fill(angle_to_rgb(i / 360))
+            pixels_1.fill(angle_to_rgb((i + 120) / 360))
+            pixels_2.fill(angle_to_rgb((i + 240) / 360))
+            pixels_0.show()
+            pixels_1.show()
+            pixels_2.show()
+            time.sleep(0.0001)
+            if button_func(button1.value, button2.value):
+                break
+
+    elif state == 4:
+        while True:
+            for i in range(360):
+                for j in range(num_pixels):
+                    pixels_0[j] = angle_to_rgb((i + 10 * j) / 360)
+                    pixels_1[j] = angle_to_rgb((i + 10 * j + 120) / 360)
+                    pixels_2[j] = angle_to_rgb((i + 10 * j + 240) / 360)
+                    pixels_0.show()
+                    pixels_1.show()
+                    pixels_2.show()
+                    time.sleep(0.0001)
+                    if button_func(button1.value, button2.value):
+                        break
+                if button_func(button1.value, button2.value, True):
+                    break
+            if button_func(button1.value, button2.value, True):
+                break
+
+    elif state == 5:
+        while True:
+            for i in range(360):
+                for j in range(num_pixels):
+                    pixels_0[j] = angle_to_rgb((i + 10 * j) / 360)
+                    pixels_1[j] = angle_to_rgb((i + 10 * j) / 360)
+                    pixels_2[j] = angle_to_rgb((i + 10 * j) / 360)
+                    pixels_0.show()
+                    pixels_1.show()
+                    pixels_2.show()
+                    time.sleep(0.0001)
+                    if button_func(button1.value, button2.value):
+                        break
+                if button_func(button1.value, button2.value, True):
+                    break
+            if button_func(button1.value, button2.value, True):
+                break
+
+    elif state == 6:
+        while True:
+            for i in range(9):
+                pixels_0.fill(angle_to_rgb(i / 9))
+                pixels_1.fill(angle_to_rgb((i + 3) / 9))
+                pixels_2.fill(angle_to_rgb((i + 6) / 9))
+
+                pixels_0.show()
+                pixels_1.show()
+                pixels_2.show()
+                time.sleep(0.1)
+                if button_func(button1.value, button2.value):
+                    break
+            if button_func(button1.value, button2.value, True):
+                break
+
+    elif state == 7:
+        for i in range(0, 512 * 10):
+            pixels_0.brightness = abs((i % 512) - 255) / 255
+            pixels_1.brightness = abs((i % 512) - 255) / 255
+            pixels_2.brightness = abs((i % 512) - 255) / 255
+            pixels_0.fill(angle_to_rgb(i / 1280))
+            pixels_1.fill(angle_to_rgb(i / 1280))
+            pixels_2.fill(angle_to_rgb(i / 1280))
+
+            pixels_0.show()
+            pixels_1.show()
             pixels_2.show()
             time.sleep(0.01)
 
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
+            if button_func(button1.value, button2.value):
                 break
 
+    elif state == 8 or state == -1:
+        pixels_0.brightness = 0
+        pixels_1.brightness = 0
+        pixels_2.brightness = 0
+        pixels_0.show()
+        pixels_1.show()
+        pixels_2.show()
+        button_func(button1.value, button2.value)
 
-    elif state == 3:
-        pass
-
-# while True:
-#     for i in range(-1000, 1000):
-#         pixels_0.brightness = abs(i) / 2550
-#         pixels_0.show()
-#         pixels_1.brightness = abs(i) / 2550
-#         pixels_1.show()
-#         pixels_2.brightness = abs(i) / 2550
-#         pixels_2.show()
-#         time.sleep(0.001)
+    time.sleep(0.3)
--- code_.py
+++ code_.py
@@ -1,5 +1,5 @@
 """
-NeoPixel example for Pico. Displays a rainbow on the NeoPixels.
+NeoPixel example for Pico. Turns the NeoPixels red.
 
 REQUIRED HARDWARE:
 * RGB NeoPixel LEDs connected to pin GP0.
@@ -7,131 +7,27 @@ REQUIRED HARDWARE:
 import time
 import board
 import neopixel
-import digitalio
+from _pixelbuf import colorwheel
 
-from rainbow import angle_to_rgb
 
 # Update this to match the number of NeoPixel LEDs connected to your board.
 num_pixels = 8
-pixels_0 = neopixel.NeoPixel(board.GP0, num_pixels, auto_write=False)
-pixels_1 = neopixel.NeoPixel(board.GP1, num_pixels, auto_write=False)
-pixels_2 = neopixel.NeoPixel(board.GP2, num_pixels, auto_write=False)
 
-pixels_0.brightness = 0.5
+pixels_1 = neopixel.NeoPixel(board.GP0, num_pixels)
 pixels_1.brightness = 0.5
+
+pixels_2 = neopixel.NeoPixel(board.GP1, num_pixels, auto_write=False)
 pixels_2.brightness = 0.5
 
-button1 = digitalio.DigitalInOut(board.GP6)
-button1.switch_to_input(pull=digitalio.Pull.UP)
-
-button2 = digitalio.DigitalInOut(board.GP7)
-button2.switch_to_input(pull=digitalio.Pull.UP)
-
-state = 1
-while True:
-    state = min(max(state, 0), 5)
-    print("state:", state)
-
-    if state == 0:
-        for i in range(360):
-            pixels_0.fill(angle_to_rgb(i / 360))
-            pixels_1.fill(angle_to_rgb(i / 360))
-            pixels_2.fill(angle_to_rgb(i / 360))
-            pixels_0.show()
-            pixels_1.show()
-            pixels_2.show()
-            time.sleep(0.0001)
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
-                break
-
-    elif state == 1:
-        pixels_0.fill((255, 100, 5))
-        pixels_1.fill((255, 100, 5))
-        pixels_2.fill((255, 100, 5))
-        for i in range(-255, 255):
-            pixels_0.brightness = abs(i) / 255 / 2
-            pixels_1.brightness = abs(i) / 255 / 2
-            pixels_2.brightness = abs(i) / 255 / 2
-            pixels_0.show()
-            pixels_1.show()
-            pixels_2.show()
-            time.sleep(0.01)
-
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
-                break
-
-    elif state == 2:
-        pixels_0.fill((255, 0, 0))
-        pixels_1.fill((0, 255, 0))
-        pixels_2.fill((0, 0, 255))
-
-        for i in range(-255, 255):
-            pixels_0.brightness = abs(i) / 255
-            pixels_1.brightness = abs(i) / 255
-            pixels_2.brightness = abs(i) / 255
-            pixels_0.show()
-            pixels_1.show()
-            pixels_2.show()
-            time.sleep(0.001)
-
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
-                break
-
-    elif state == 3:
-        for i in range(360):
-            pixels_0.fill(angle_to_rgb(i / 360))
-            pixels_1.fill(angle_to_rgb((i + 120) / 360))
-            pixels_2.fill(angle_to_rgb((i + 240) / 360))
-            pixels_0.show()
-            pixels_1.show()
-            pixels_2.show()
-            time.sleep(0.0001)
-            if button1.value == 0:
-                state += 1
-                break
-            if button2.value == 0:
-                state -= 1
-                break
-
-    elif state == 4:
-        while True:
-            for i in range(9):
-                pixels_0.fill(angle_to_rgb(i / 9))
-                pixels_1.fill(angle_to_rgb((i + 3) / 9))
-                pixels_2.fill(angle_to_rgb((i + 6) / 9))
-                pixels_0.show()
-                pixels_1.show()
-                pixels_2.show()
-                time.sleep(0.1)
-                if button1.value == 0:
-                    state += 1
-                    break
-                if button2.value == 0:
-                    state -= 1
-                    break
-            if button1.value == 0 or button2.value == 0:
-                break
-
-    elif state == 5:
-        pixels_0.brightness = 0
-        pixels_1.brightness = 0
-        pixels_2.brightness = 0
-        pixels_0.show()
-        pixels_1.show()
+def rainbow(speed):
+    for j in range(255):
+        for i in range(num_pixels):
+            pixel_index = (i * 256 // num_pixels) + j
+            pixels_2[i] = colorwheel(pixel_index & 255)
         pixels_2.show()
-        if button2.value == 0:
-            state -= 1
+        time.sleep(speed)
 
-    time.sleep(0.3)
+while True:
+    pixels_1.fill((255, 0, 0))
+    rainbow(0)
+
